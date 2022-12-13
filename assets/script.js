47820f877a7ba1cfb1dd0feea725d90f4f18edaa
jennahopeclem <jennahopeclem@gmail.com>
2022-12-12 19:53:36 -0500

    saving--need readme

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,10 @@
 
 function weather(citySearch) {
     var cityName = $(".city-name")
-    // cityName = citySearch.value
-    // console.log($(".city-name"));
-    // console.log(citySearch);
 
     var weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&units=imperial&appid=afa1a88a95052b1eef7ecb94fe170eb6`;
 
-    fetch(weatherUrl).then(res => { // same as function(response)
+    fetch(weatherUrl).then(res => {
         return res.json();
     })
         .then(data => {
@@ -58,7 +55,6 @@ $('#search-btn').on('click', search);
 function search() {
     var citySearch = $("#search-city").val().trim();
     weather(citySearch);
-    // forecast(citySearch);
     addCity(citySearch);
     addSearchButtons();
     forecast(citySearch);
@@ -78,31 +74,8 @@ function addSearchButtons() {
             forecast(city);
         })
         searchEl.append(button);
-        //set butt on text to city ; append button to end of search 
     }
 }
 
 addSearchButtons();
 
-
-// $data.list[0].main.temp
-
-
-
-//currentWeather
-//fetch API
-//get data for temperature, humidity and wind; add weather icon; need date
-//display data-create list through .innerHTML
-//set localStorage here
-
-//5 day weather
-//fetch API
-//get data for temperature, humidity and wind; add weather icon; need date
-//use for loop to get 5 days 
-//i++ ?
-//display data-create list through .innerHTML
-
-//search history - localStorage
-//get from input or data - search history should be there always (on start-up) hint* with localStorage, don't save all info; just save 'name'
-//search history = localStorage but how many? (5)
-//click on search history and get city info
